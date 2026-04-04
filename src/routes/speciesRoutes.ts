7a04918d3fe96e0c01efb5cacdeaaa735fd55cd8
Mubaraq Olayinka <mubaraqolayinka85@gmail.com>
2026-04-04 17:54:13 +0100

    create specie

--- src/routes/speciesRoutes.ts
+++ src/routes/speciesRoutes.ts
@@ -1,11 +1,12 @@
 import { Router } from "express";
 import * as controller from "../controllers/speciesController";
-import { verifyToken } from "../middleware/auth";
+import { requireRole, verifyToken } from "../middleware/auth";
 
 const router = Router();
 
 router.get("/", controller.getAll);
 router.get("/org", verifyToken, controller.getByOrganization);
 router.get("/:id", controller.getById);
+router.post("/", verifyToken, requireRole('teacher'), controller.create);
 
 export default router;
